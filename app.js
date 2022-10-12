5472e0f4b89a3a2d7318509d30a455d60299ba5a
Arturo Ramirez <86683854+arturo-ramirez-civ@users.noreply.github.com>
2022-10-12 17:42:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+//create variable to select IDs
+const form = document.querySelector("#dog");
+const input = document.querySelector("#dogName");
+const list = document.querySelector("#list");
 
+//add event listener
+form.addEventListener("submit", function (e){
+    //prevent the form from submitting
+    e.preventDefault();
+    //saves input into a variable
+    const dogName = input.value;
+    //creates a new list item in the html
+    const newLI = document.createElement("LI"); 
+    //sets the list item value to the value of dogName variable
+    newLI.innerText = dogName;
+    //displays dogName values
+    list.append(newLI);
+
+    dogName.reset();
+});
